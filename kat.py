a880ee8ed310d9d7a022183323a9d686746f05d8
Matthew Thorley <thorley@gmail.com>
2023-02-24 23:17:15 -0700

    Sophia's first program

---
+++ kat.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from time import sleep
+import random
+kat ="""
+      |\      _,,,---,,_
+ZZZzz /,`.-'`'    -.  ;-;;,_
+     |,4-  ) )-,_. ,\ (  `'-'
+    '---''(_/--'  `-'\_)
+"""
+dino='''
+           boing         boing         boing
+ e-e           . - .         . - .         . - .
+(\_/)\       '       `.   ,'       `.   ,'       .
+ `-'\ `--.___,         . .           . .          .
+    '\( ,_.-'
+       \\               "             "            a:f
+       ^'
+'''
+words="    Sophia is smarter than Albert Einstien! >:D"
+
+def draw (s,t=0.1):
+    lines=s.splitlines()
+    for l in lines:
+        for c in l:
+            print (c, end="", flush=True)
+            sleep(t)
+        print()
+
+pic=random.choice([dino,kat])
+draw(pic)
+draw (words,0.3)
